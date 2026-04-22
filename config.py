fdb4f0b5968531d00269951941c7da6afa4b3406
mayajinshi <mayajinshi@gmail.com>
2026-04-22 10:07:31 +0530

    Add default configurations and URL detection regex
    Added default warning limit, punishment options, and URL regex pattern.

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import re
 
 load_dotenv()
 
@@ -16,3 +17,11 @@ BOT_USERNAME = os.getenv("BOT_USERNAME","")
 SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "")
 UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "")
 START_IMAGE = os.getenv("START_IMAGE", "")
+DEFAULT_WARNING_LIMIT = 3
+DEFAULT_PUNISHMENT = "mute" # Options: "mute", "ban"
+DEFAULT_CONFIG = ("warn", DEFAULT_WARNING_LIMIT, DEFAULT_PUNISHMENT)
+
+# Regex pattern to detect URLs in user bios
+URL_PATTERN = re.compile(
+    r'(https?://|www\.)[a-zA-Z0-9.\-]+(\.[a-zA-Z]{2,})+(/[a-zA-Z0-9._%+-]*)*' #done change here
+)
